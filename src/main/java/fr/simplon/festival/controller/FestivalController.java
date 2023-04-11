5574a9d368742598f925ec47706fa4e6ba21afc4
Ethan <121941436+VirapinEthan@users.noreply.github.com>
2023-04-11 11:48:19 +0200

    commit

--- src/main/java/fr/simplon/festival/controller/FestivalController.java
+++ src/main/java/fr/simplon/festival/controller/FestivalController.java
@@ -8,10 +8,6 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class FestivalController {
 
-    @Autowired
-    private FestivalDao festivalDao;
 
-    @Autowired
-    private FestivalDaoImpl festivalDaoImpl;
 
 }
