f45f008813eb9419ae27de9abf1a05cd3606e373
MaiwenM <121941644+MaiwenM@users.noreply.github.com>
2023-04-13 14:04:27 +0200

    avancement 13/04

--- src/main/java/fr/simplon/festival/dao/FestivalDao.java
+++ src/main/java/fr/simplon/festival/dao/FestivalDao.java
@@ -2,12 +2,13 @@ package fr.simplon.festival.dao;
 
 import fr.simplon.festival.entity.Festival;
 
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 
 public interface FestivalDao {
 
-    void saveFestival(String nom, String url, Date debut, Date fin, String ville, int cp, String lieu, double lat, double lon);
+    void saveFestival(String nom, String url, LocalDate debut, LocalDate fin, String ville, int cp, String lieu, double lat, double lon);
 
     List<Festival> getAllFestivals();
 }
